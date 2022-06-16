9bb5364fbec9b0ad2a389f95c37ce47f66cd80d7
Emir Güney Yavuzkaplan <emirguneyyavuzkaplan@gmail.com>
2022-06-16 14:14:49 +0300

    core Mvc giris

--- CoreMvcGiris/Contollers/HomeController.cs
+++ CoreMvcGiris/Contollers/HomeController.cs
@@ -1,10 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoreMvcGiris.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
-namespace CoreMvcGiris.Contollers
+namespace CoreMvcGiris.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
+        {
+            List<Kisi> kisiler = new List<Kisi>();
+            Kisi ali = new Kisi { Ad = "Ali", Soyad = "Yilmaz", Gsm = "532" };
+            kisiler.Add(ali);
+            Kisi ayse = new Kisi { Ad = "Ayse", Soyad = "Kaya", Gsm = "542" };
+            kisiler.Add(ayse);
+            Kisi fatma = new Kisi { Ad = "Fatma", Soyad = "Erturk", Gsm = "544" };
+            kisiler.Add(fatma);
+
+            return View(kisiler);
+        }
+        public IActionResult Hakkimizda()
+        {
+            return View();
+        }
+
+        public IActionResult Referanslar()
         {
             return View();
         }
--- CoreMvcGiris/Startup.cs
+++ CoreMvcGiris/Startup.cs
@@ -16,6 +16,7 @@ namespace CoreMvcGiris
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
